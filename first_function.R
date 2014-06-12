90ed37c32c62a3495dc3cd643f289cbe90d567de
vikramranga <vikramranga@users.noreply.github.com>
2014-06-12 14:29:49 +0530

    Update first_function.R

--- first_function.R
+++ first_function.R
@@ -7,3 +7,13 @@ conversion <- function (x)
 	sec. <- (as.numeric(paste(deg.val.[3], deg.val.[4], sep = ".")))/3600
 	return(deg. + mins + sec.)
 }
+
+
+# Outlier deletion
+
+out.del <- function(x){
+  lo.bound <- quantile(x)[2] - (IQR(x) * 1.5)
+  up.bound <- quantile(x)[4] + (IQR(x) * 1.5)
+  x <- x[x >= lo.bound & x <= up.bound]
+  return(x)
+}
